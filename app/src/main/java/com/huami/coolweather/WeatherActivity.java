1e3c8d1a871c29c55adb12fe1717f72ffab9f7fd
wangjinhan <wangjinhan@huami.com>
2018-12-26 16:34:19 +0800

    初步功能已经完成

--- app/src/main/java/com/huami/coolweather/WeatherActivity.java
+++ app/src/main/java/com/huami/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.huami.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.huami.coolweather.gson.Forecast;
 import com.huami.coolweather.gson.Weather;
+import com.huami.coolweather.service.AutoUpdateService;
 import com.huami.coolweather.util.HttpUtil;
 import com.huami.coolweather.util.Utility;
 
@@ -208,6 +210,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     private void loadBingPic() {
